4f440de266f6e8c47625ced58a573c1757537a16
temujins <mdsifatulislam.rabbi@gmail.com>
2021-06-24 21:51:26 +0600

    complete

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Hero, Products, Featured } from "../../Components"
-import { Practice } from "../../Components"
 
 const HomePage = ({ data }) => {
    return (
@@ -9,7 +8,6 @@ const HomePage = ({ data }) => {
          <Products data={data.pizza_data} heading="Choose Your Favorites" />
          <Featured data={data.pizza_of_the_day} heading="Pizza of the Day" />
          <Products data={data.sweets} heading="Sweet Treats For You" />
-         <Practice />
       </>
    )
 }
